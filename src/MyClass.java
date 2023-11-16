3556a1602ebbba0799616ec67678095a4b8fc230
katetsukerka <tsukerka91@gmail.com>
2023-11-16 18:24:40 +0200

    HW#1

---
+++ src/MyClass.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    static int sumOfTwoNumbers(int a, int b) {
+        int c = a + b; //не розумію чому a,b,c підкреслені
+        return c;
+    }
+
+    public static void main(String[] args) {
+        int c = sumOfTwoNumbers(1, 2);
+        System.out.println(c);
+    }
+}
+
